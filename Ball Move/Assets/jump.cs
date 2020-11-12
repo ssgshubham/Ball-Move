82f05e03333167ea0582225ed013800fd728aec9
ssgshubham <62928567+ssgshubham@users.noreply.github.com>
2020-11-13 04:17:18 +0530

    bm

---
+++ Ball Move/Assets/jump.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jump : MonoBehaviour
+{
+    public bool onground;
+    private Rigidbody rb;
+    public float jumpf;
+
+    void Start()
+    {
+        onground = true;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (onground)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                jumpb();
+            }
+        }
+    }
+
+    public void jumpb()
+    {
+        rb.velocity = new Vector3(0f, jumpf, 0f);
+        onground = false;
+    }
+
+    void OnCollisionEnter(Collision Other)
+    {
+        if (Other.gameObject.CompareTag("Ground"))
+        {
+            onground = true;
+        }
+    }
+
+    
+
+}
--- Ball Move/Assets/movement.cs
+++ Ball Move/Assets/movement.cs
@@ -1,85 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 public class movement : MonoBehaviour
 {
+	//variables
+	public float moveSpeed = 300;
+	public GameObject character;
 
-    bool moveRight = false, moveLeft = false;
-    [SerializeField] Rigidbody rb;
-    [SerializeField] Camera c;
-    GameObject touchedObject;
-    RaycastHit hit;
-
-
+	private Rigidbody characterBody;
+	private float ScreenWidth;
 
 
-    void Update()
-    {
-        //prevent Double Jump
-        var Grounded = true;
-        Grounded = Physics.Raycast(transform.position - Vector3.up * .2f, Vector3.down, .09f);
+	// Use this for initialization
+	void Start()
+	{
+		ScreenWidth = Screen.width;
+		characterBody = character.GetComponent<Rigidbody>();
+	}
 
-        //check the touched object is ball or not
-        if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began)
-        {
-            Ray ray = c.ScreenPointToRay(Input.GetTouch(0).position);
-            if (Physics.Raycast(ray, out hit))
-            {
-                if (hit.collider != null)
-                {
-                    touchedObject = hit.transform.gameObject;
-                    if (touchedObject.transform.name == "Soccer Ball" && Grounded)
-                    {
-                        rb.velocity = new Vector3(0, 5f, 0);
+	// Update is called once per frame
+	void Update()
+	{
+		int i = 0;
+		//loop over every touch found
+		while (i < Input.touchCount)
+		{
+			if (Input.GetTouch(i).position.x > ScreenWidth / 2)
+			{
+				//move right
+				RunCharacter(1.0f);
+			}
+			if (Input.GetTouch(i).position.x < ScreenWidth / 2)
+			{
+				//move left
+				RunCharacter(-1.0f);
+			}
+			++i;
+		}
+	}
+	void FixedUpdate()
+	{
+#if UNITY_EDITOR
+		RunCharacter(Input.GetAxis("Horizontal"));
+#endif
+	}
 
+	private void RunCharacter(float horizontalInput)
+	{
+		//move player
+		characterBody.AddForce(new Vector2(horizontalInput * moveSpeed * Time.deltaTime, 0));
 
-                    }
-                }
+	}
 
-            }
-        }
-
-        //movement
-        if (Grounded)//prevent air control
-        {
-            if (moveRight == true && touchedObject.transform.name != "Soccer Ball")
-            {
-                rb.velocity = new Vector3(Time.deltaTime * 100, 0, 0);
-
-            }
-            if (moveRight == false && moveLeft == false)
-            {
-                rb.velocity = new Vector3(Time.deltaTime * 00, rb.velocity.y, 0);
-            }
-
-            if (moveLeft == true && touchedObject.transform.name != "Soccer Ball")
-            {
-
-                rb.velocity = new Vector3(-Time.deltaTime * 100, 0, 0);
-
-            }
-
-            transform.position = new Vector3(Mathf.Clamp(transform.position.x, -2.8f, 2.8f), transform.position.y, transform.position.z);
-        }
-
-    }
-
-    public void MoveRight()
-    {
-        moveRight = true;
-    }
-
-    public void notMoveRight()
-    {
-        moveRight = false;
-    }
-    public void MoveLeft()
-    {
-        moveLeft = true;
-    }
-
-    public void notMoveLeft()
-    {
-        moveLeft = false;
-    }
 }
